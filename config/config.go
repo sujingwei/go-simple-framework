75f50b58bf4cac30c59f7786097433e5c54acdaa
sujingwei <348149047@qq.com>
2024-03-13 13:47:42 +0800

    11

--- config/config.go
+++ config/config.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: sujingwei 348149047@qq.com
+ * @Date: 2024-03-12 19:13:05
+ * @LastEditors: sujingwei 348149047@qq.com
+ * @LastEditTime: 2024-03-12 20:37:16
+ * @FilePath: \go-simple-framework\config\config.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package config
 
 import (
@@ -6,6 +14,6 @@ import (
 )
 
 type AppConfigure struct {
-	configuration.App `yaml: "app"`
-	Web               webframework.WebConfig `yaml: "web`
+	App configuration.App      `yaml: "app"`
+	Web webframework.WebConfig `yaml: "web`
 }
