a9764a43fc445c01bad0210ee5a4d9af6912bfe9
sujingwei <348149047@qq.com>
2024-03-13 20:32:01 +0800

    同步

--- web-framework/bootstrap.go
+++ web-framework/bootstrap.go
@@ -2,7 +2,7 @@
  * @Author: sujingwei 348149047@qq.com
  * @Date: 2024-03-10 12:25:06
  * @LastEditors: sujingwei 348149047@qq.com
- * @LastEditTime: 2024-03-13 20:29:11
+ * @LastEditTime: 2024-03-13 20:31:28
  * @FilePath: \go-simple-framework\web-framework\bootstrap.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
